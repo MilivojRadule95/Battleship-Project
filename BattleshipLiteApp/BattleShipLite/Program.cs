2e5d190063a66c09bc09ffc401a30ab428bb5658
DESKTOP-D84HH4L\Milivoj <milivoj.raodnic@gmail.com>
2022-01-13 23:12:10 +0100

    In the beginning

---
+++ BattleshipLiteApp/BattleShipLite/Program.cs
@@ -0,0 +1,41 @@
+﻿using BattleshipLiteLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleShipLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeMessage();
+            Console.ReadLine();
+        }
+        private static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to the Battleship Lite");
+            Console.WriteLine("Created by Milivoj Radonic");
+            Console.WriteLine();
+        }
+        private static PlayerInfoModel CreatePlayer()
+        {
+            PlayerInfoModel output = new PlayerInfoModel();
+
+            //Ask user for their name
+            output.UsersName = AskForUsersName();
+            //Load up the shot grid
+            output.ShotGrid
+            //Ask the user for their 5 ships placments
+            //Clear
+        }
+        private static string AskForUsersName()
+        {
+            Console.Write("What is yiur name: ");
+            string output = Console.ReadLine();
+            return output;
+        }
+    }
+}
---
+++ BattleshipLiteApp/BattleshipLiteLibrary/GameLogic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleshipLiteLibrary
+{
+    public static class GameLogic //here is static class because there's no need to store dati inside it. 
+    {                             //It will only contain logic methods that can be reused.
+    }
+}
