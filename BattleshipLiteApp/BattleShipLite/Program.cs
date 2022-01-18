94e33da93aeedd916366733d63509de6a6a4f4d7
DESKTOP-D84HH4L\Milivoj <milivoj.raodnic@gmail.com>
2022-01-18 18:24:26 +0100

    half way through

--- BattleshipLiteApp/BattleShipLite/Program.cs
+++ BattleshipLiteApp/BattleShipLite/Program.cs
@@ -1,4 +1,5 @@
-﻿using BattleshipLiteLibrary.Models;
+﻿using BattleshipLiteLibrary;
+using BattleshipLiteLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,10 @@ namespace BattleShipLite
         static void Main(string[] args)
         {
             WelcomeMessage();
+
+            PlayerInfoModel player1 = CreatePlayer("Player 1");
+            PlayerInfoModel player2 = CreatePlayer("Player 2");
+
             Console.ReadLine();
         }
         private static void WelcomeMessage()
@@ -20,22 +25,45 @@ namespace BattleShipLite
             Console.WriteLine("Created by Milivoj Radonic");
             Console.WriteLine();
         }
-        private static PlayerInfoModel CreatePlayer()
+        private static PlayerInfoModel CreatePlayer(string playerTitle)
         {
             PlayerInfoModel output = new PlayerInfoModel();
+
+            Console.WriteLine($"Player information for {playerTitle}");
 
             //Ask user for their name
             output.UsersName = AskForUsersName();
             //Load up the shot grid
-            output.ShotGrid
+            GameLogic.InitalizeGrid(output);
             //Ask the user for their 5 ships placments
+            ShipPlacment(output);
             //Clear
+            Console.Clear();
+
+            return output;
         }
         private static string AskForUsersName()
         {
-            Console.Write("What is yiur name: ");
+            Console.Write("What is your name: ");
             string output = Console.ReadLine();
+
             return output;
+        }
+        private static void ShipPlacment(PlayerInfoModel model)
+        {
+            do
+            {
+                Console.Write($"Where do you want to place your ship number {model.ShipLocations.Count + 1}: ");
+                string location = Console.ReadLine();
+
+                bool isValidLocation = GameLogic.PlaceShip(model, location);
+
+                if (isValidLocation == false)
+                {
+                    Console.WriteLine("That was not a valid location, please try again.");
+                }
+
+            } while (model.ShipLocations.Count < 5);
         }
     }
 }
--- BattleshipLiteApp/BattleshipLiteLibrary/GameLogic.cs
+++ BattleshipLiteApp/BattleshipLiteLibrary/GameLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BattleshipLiteLibrary.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,51 @@ using System.Threading.Tasks;
 
 namespace BattleshipLiteLibrary
 {
-    public static class GameLogic //here is static class because there's no need to store dati inside it. 
-    {                             //It will only contain logic methods that can be reused.
+    public static class GameLogic //here is static class because there's no need to store data inside it. 
+    {   //It will only contain logic methods that can be reused.
+        public static void InitalizeGrid(PlayerInfoModel model)
+        {
+            List<string> letters = new List<string>
+            {
+                "A",
+                "B",
+                "C",
+                "D",
+                "E",
+               
+            };
+            List<int> numbers = new List<int>
+            {
+                1,
+                2,
+                3,
+                4,
+                5
+            };
+            foreach (string letter in letters)
+            {
+                foreach (int number in numbers)
+                {
+                    AddGridSpot(model, letter, number);
+                }
+
+            }
+        }
+        private static void AddGridSpot(PlayerInfoModel model, string letter, int number)
+        {
+            GridSpotModel spot = new GridSpotModel
+            {
+                SpotLetter = letter,
+                SpotNumber = number,
+                Status = GridSpotStatus.Empty
+            };
+            
+            model.ShotGrid.Add(spot);
+        }
+
+        public static bool PlaceShip(PlayerInfoModel model, string location)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
