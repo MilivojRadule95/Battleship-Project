f0d81a0a4d3442b218e1d4335aeb1e3cfd5327de
DESKTOP-D84HH4L\Milivoj <milivoj.raodnic@gmail.com>
2022-01-19 23:30:33 +0100

    logic design

--- BattleshipLiteApp/BattleShipLite/Program.cs
+++ BattleshipLiteApp/BattleShipLite/Program.cs
@@ -22,15 +22,83 @@ namespace BattleShipLite
             {
                 // Display grid from activePlayer on where they fired
                 DisplayShotGrid(activePlayer);
+
                 // Ask activePlayer for a shot
                 // Determine if it's a valid shot(keep looping until he gives a valid shot)
                 // Determine shot result
-                // Determine if the game is over
+                RecrodPlayerShot(activePlayer, opponent);
+
+                // Determine if the game is over -> do that in the GameLogic
+                bool doesGameContinue = GameLogic.PlayerStillActive(opponent);
+
                 // If over set activePlayer as the winner,
                 // else, swap positions (activePlayer to opponent)
+                if (doesGameContinue==true)
+                {
+                    //use tuple or temporary variable to swap positions??
+
+                    //SWAP USING TEMP VARIABLE:
+                    PlayerInfoModel tempHolder = opponent;
+                    opponent = activePlayer;
+                    activePlayer = tempHolder;
+                    
+                    //swap using tuple:
+                    (activePlayer, opponent) = (opponent, activePlayer);
+                }
+                else
+                {
+                    winner = activePlayer;
+                }
+
             } while (winner == null);
+            IdentifyTWinner(winner);
 
             Console.ReadLine();
+        }
+
+        private static void IdentifyTWinner(PlayerInfoModel winner)
+        {
+            Console.WriteLine($"Congratulations to {winner.UsersName} for winning");
+            Console.WriteLine($"{winner.UsersName} took {GameLogic.GetShotCount(winner)} shots.");
+        }
+
+        private static void RecrodPlayerShot(PlayerInfoModel activePlayer, PlayerInfoModel opponent)
+        {
+            //Ask for a shot (do we ask for "B2" or "B" and then "2")
+            //Determine what row and column that is - split it apart
+            //Determine if that is a valid shot
+            //Go back to beginnig if not a valid shot
+            bool isValidShoot = false;
+            string row = "";
+            int column = 0;
+
+            do
+            {
+                string shot = AskForShot();
+                ( row, column) = GameLogic.SplitShotIntoRowAndColum(shot);
+                isValidShoot = GameLogic.ValidateShot(activePlayer, row, column);
+
+                if (isValidShoot == false)
+                {
+                    Console.WriteLine("Invalid shot location, please try again.");
+                }
+
+            } while (isValidShoot == false);
+
+
+            //Determine the results 
+            bool isAHit = GameLogic.IdentifyShotResult(opponent, row, column);
+
+            //Record results
+            GameLogic.MarkShotResult(activePlayer, row, column, isAHit); 
+        }
+
+        private static string AskForShot()
+        {
+            Console.WriteLine($"Please enter your shot selection: ");
+            string output = Console.ReadLine();
+
+            return output; 
         }
 
         private static void DisplayShotGrid(PlayerInfoModel activePlayer)
